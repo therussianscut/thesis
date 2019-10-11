1745f86eefd1369be668d94f092c430181627132
therussianscut <42698688+therussianscut@users.noreply.github.com>
2019-10-11 20:45:33 +0000

    first work on User management view

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+
+Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
+
+
+
+});
